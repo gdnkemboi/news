fcffea297f03313639569d847d8c75c190b4a4f2
Gideon Kemboi <gedionkemboi18@gmail.com>
2023-07-27 13:59:08 +0300

    fixed nameerror

--- django_project/settings.py
+++ django_project/settings.py
@@ -14,11 +14,11 @@ from pathlib import Path
 from environs import Env
 
 env = Env()
-env.read_env(BASE_DIR / ".env")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
