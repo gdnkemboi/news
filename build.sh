f8f0827cf9425543a9a0100c43b90a976804dcd1
Gideon Kemboi <gedionkemboi18@gmail.com>
2023-07-28 13:22:28 +0300

    fixed build script

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-python manage.py createsuperuser --no-input
 python manage.py migrate
+python manage.py createsuperuser --no-input
 
 if [[ $CREATE_SUPERUSER ]];
 then
